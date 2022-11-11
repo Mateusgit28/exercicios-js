2016a71f2e60db7212f06ad1c482dec42c948a1c
Mateusgit28 <110639736+Mateusgit28@users.noreply.github.com>
2022-11-10 22:25:16 -0300

    exercicios

---
+++ 004/app.js
@@ -0,0 +1,4 @@
+let nota= document.getElementById('n1'). value
+ let nota= document.getElementById('n2'). value
+ let nota= document.getElementById('n1'). value
+ let nota= document.getElementById('n2'). value
